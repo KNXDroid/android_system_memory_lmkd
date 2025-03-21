0f97d8993aea50da8e4a0862afde93ff954d7938
Tomislav Novak <tnovak@meta.com>
2025-03-21 12:03:56 -0700

    lmkd: Fix state initialization in __mp_event_psi
    Some globals like base_file_lru are currently initialized only after
    the first kill. Thrashing percentage, which is based on base_file_lru,
    can therefore be calculated incorrectly the first time mp_event_psi()
    is called, causing lmkd to erroneously kill a process:
    
      Kill [...] reason: device is in direct reclaim and thrashing (101812%)
    
    Test: log base_file_lru and verify that it's always non-zero
    Change-Id: Ibb973fe82bdf5522ea4b02df90fa3524af6f3122
    Signed-off-by: Tomislav Novak <tnovak@meta.com>

--- lmkd.cpp
+++ lmkd.cpp
@@ -2707,6 +2707,7 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
     static int64_t prev_thrash_growth = 0;
     static bool check_filecache = false;
     static int max_thrashing = 0;
+    static bool initialized = false;
 
     union meminfo mi;
     union vmstat vs;
@@ -2763,15 +2764,18 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
         return;
     }
 
-    /* Reset states after process got killed */
-    if (killing) {
-        killing = false;
-        cycle_after_kill = true;
+    /* Initialize states the first time we get here and reset after a kill */
+    if (!initialized || killing) {
+        if (killing) {
+            killing = false;
+            cycle_after_kill = true;
+        }
         /* Reset file-backed pagecache size and refault amounts after a kill */
         base_file_lru = vs.field.nr_inactive_file + vs.field.nr_active_file;
         init_ws_refault = workingset_refault_file;
         thrashing_reset_tm = curr_tm;
         prev_thrash_growth = 0;
+        initialized = true;
     }
 
     /* Check free swap levels */
